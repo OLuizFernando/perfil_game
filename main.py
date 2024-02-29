83af3615d2e110d1e3eb0b4096b7673be823a382
Luiz Fernando <luizfernandodematoscarvalho@gmail.com>
2024-02-29 00:11:12 -0300

    nova_carta function

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import pandas as pd
 from tkinter import *
+from random import randint
+
+
+def nova_carta():
+
+    canvas.delete('all')
+    botao_iniciar.destroy()
+
+    carta_escolhida = cartas.iloc[randint(0, len(cartas) - 1)]
+    canvas.create_text(350, 50, text=f'Categoria: {carta_escolhida.categoria}', fill=AMARELO,
+                       font=('Times', 30, 'bold'))
+    canvas.create_text(350, 100, text=f'1 - {carta_escolhida.dica1}', fill=AMARELO,
+                       font=('Times', 15))
+
 
 AMARELO = '#FBA834'
 AZUL = '#387ADF'
@@ -14,13 +28,13 @@ janela.title('Perfil - Python Edition')
 janela.resizable(width=FALSE, height=FALSE)
 janela.config(pady=50, padx=50, bg=AZUL_ESCURO)
 
-canvas = Canvas(width=520, height=220, bg=AZUL_ESCURO, highlightthickness=0)
-canvas.create_text(260, 100, text='PERFIL', fill=AMARELO, font=('Times', 100, 'bold'))
-canvas.create_text(260, 175, text='Python Edition', fill=AMARELO, font=('Times', 40))
+canvas = Canvas(width=700, height=220, bg=AZUL_ESCURO, highlightthickness=0)
+texto1 = canvas.create_text(350, 100, text='PERFIL', fill=AMARELO, font=('Times', 100, 'bold'))
+texto2 = canvas.create_text(350, 175, text='Python Edition', fill=AMARELO, font=('Times', 40))
 canvas.grid(column=0, row=0)
 
 botao_iniciar = Button(janela, text='Iniciar', font=('Times', 20, 'bold'),
-                       width=10, height=2, foreground=AZUL_ESCURO)
+                       width=10, height=2, foreground=AZUL_ESCURO, command=nova_carta)
 botao_iniciar.grid(column=0, row=1, pady=50)
 
 janela.mainloop()
